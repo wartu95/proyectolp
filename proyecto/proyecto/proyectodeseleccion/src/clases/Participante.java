8b410899ca7371c24c15cee9e573ae8b08638b27
klinsmann726 <129577225+klinsmann726@users.noreply.github.com>
2023-04-01 08:57:30 -0500

    actualizadoKlinsmann

--- proyecto/proyecto/proyectodeseleccion/src/clases/Participante.java
+++ proyecto/proyecto/proyectodeseleccion/src/clases/Participante.java
@@ -6,57 +6,101 @@ public class Participante {
 	private String Apellido;
 	private String Nombre;
 	private String Dni;
-	private int Felefono;
+	private int Telefono;
 	private String Correo;
-	//
+	
+	
+	
 	public Participante() {
-		
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	public Participante(int idParticipante, String apellido, String nombre, String dni, int felefono, String correo) {
+
+
+
+	public Participante(int idParticipante, String apellido, String nombre, String dni, int telefono, String correo) {
 		super();
 		this.idParticipante = idParticipante;
-		this.Apellido = apellido;
-		this.Nombre = nombre;
-		this.Dni = dni;
-		this.Felefono = felefono;
-		this.Correo = correo;
-	}
-	public int getIdParticipante() {
-		return idParticipante;
-	}
-	public void setIdParticipante(int idParticipante) {
-		this.idParticipante = idParticipante;
-	}
-	public String getApellido() {
-		return Apellido;
-	}
-	public void setApellido(String apellido) {
 		Apellido = apellido;
-	}
-	public String getNombre() {
-		return Nombre;
-	}
-	public void setNombre(String nombre) {
 		Nombre = nombre;
-	}
-	public String getDni() {
-		return Dni;
-	}
-	public void setDni(String dni) {
 		Dni = dni;
-	}
-	public int getFelefono() {
-		return Felefono;
-	}
-	public void setFelefono(int felefono) {
-		Felefono = felefono;
-	}
-	public String getCorreo() {
-		return Correo;
-	}
-	public void setCorreo(String correo) {
+		Telefono = telefono;
 		Correo = correo;
 	}
 
+
+
+	public int getIdParticipante() {
+		return idParticipante;
+	}
+
+
+
+	public void setIdParticipante(int idParticipante) {
+		this.idParticipante = idParticipante;
+	}
+
+
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+
+
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+
+
+	public String getDni() {
+		return Dni;
+	}
+
+
+
+	public void setDni(String dni) {
+		Dni = dni;
+	}
+
+
+
+	public int getTelefono() {
+		return Telefono;
+	}
+
+
+
+	public void setTelefono(int telefono) {
+		Telefono = telefono;
+	}
+
+
+
+	public String getCorreo() {
+		return Correo;
+	}
+
+
+
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
+	
+	
+	
 }
 	
