6d94ccf10e4a8f0eb049536684c8195289c11103
JCarlos1609 <129580213+JCarlos1609@users.noreply.github.com>
2023-04-01 05:06:30 -0500

    Clases actualizadas

--- proyecto/proyecto/proyectodeseleccion/src/clases/Usuario.java
+++ proyecto/proyecto/proyectodeseleccion/src/clases/Usuario.java
@@ -1,5 +1,62 @@
 package clases;
 
 public class Usuario {
-
+// 
+	private String idUsuario;
+	private String Contraseña;
+	private String Nombre;
+	private String Apellido;
+	private int idCargo;
+	private int idPerfil;
+	//
+	public Usuario() {
+	
+	}
+	public Usuario(String idUsuario, String contraseña, String nombre, String apellido, int idCargo, int idPerfil) {
+		super();
+		this.idUsuario = idUsuario;
+		this.Contraseña = contraseña;
+		this.Nombre = nombre;
+		this.Apellido = apellido;
+		this.idCargo = idCargo;
+		this.idPerfil = idPerfil;
+	}
+	//get set
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getContraseña() {
+		return Contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		Contraseña = contraseña;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public int getIdCargo() {
+		return idCargo;
+	}
+	public void setIdCargo(int idCargo) {
+		this.idCargo = idCargo;
+	}
+	public int getIdPerfil() {
+		return idPerfil;
+	}
+	public void setIdPerfil(int idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+	
 }
